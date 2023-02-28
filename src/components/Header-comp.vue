c97893c5b72c5dbb266d624a12f97018a6a213c2
Christian Ørum <chri@bubbles.dk>
2023-02-28 13:09:53 +0100

    Added media query + started phone menu

--- src/components/Header-comp.vue
+++ src/components/Header-comp.vue
@@ -6,17 +6,47 @@
             <img class="w-fit h-full" alt="CARDlogo-white" src="@/assets/cardlogo_whiteout.svg"/>
         </div>
 
-        <div class="h-2/5 absolute right-[55px] flex text-white flex-row items-center gap-7 text-xl">
-            <a class="font-Poppins font-normal text-[16px] text-white hover:text-[#F8D027] transition-all ease-out duration-300">Om foreningen</a>
-            <a class="font-Poppins font-normal text-[16px] text-white hover:text-[#F8D027] transition-all ease-out duration-300">Begivenheder</a>
-            <a class="font-Poppins font-normal text-[16px] text-white hover:text-[#F8D027] transition-all ease-out duration-300">Patches</a>
-            <a href="#_" class="font-Poppins font-normal text-[16px] relative rounded-xl px-5 py-2 overflow-hidden group bg-[#040900] outline-3 outline outline-transparent outline-offset-4 border-4 border-[#F8D027] relative hover:bg-gradient-to-r hover:from-[#040900] hover:to-[#2C2801] hover:outline-3 hover:outline-[#F8D027]/[0.25] hover:outline hover:outline-offset-4 text-white transition-all ease-out duration-300">
+        <div  v-if="screenW > 1024" class="h-2/5 absolute right-[55px] flex text-white flex-row items-center gap-7 text-xl">
+            <a class="font-Poppins font-normal text-[14px] lg:text-[16px] text-white hover:text-[#F8D027] transition-all ease-out duration-300">Om foreningen</a>
+            <a class="font-Poppins font-normal text-[14px] lg:text-[16px] text-white hover:text-[#F8D027] transition-all ease-out duration-300">Begivenheder</a>
+            <a class="font-Poppins font-normal text-[14px] lg:text-[16px] text-white hover:text-[#F8D027] transition-all ease-out duration-300">Patches</a>
+            <a href="#_" class="font-Poppins font-normal text-[14px] lg:text-[16px] relative rounded-xl px-5 py-2 overflow-hidden group bg-[#040900] outline-3 outline outline-transparent outline-offset-4 border-4 border-[#F8D027] relative hover:bg-gradient-to-r hover:from-[#040900] hover:to-[#2C2801] hover:outline-3 hover:outline-[#F8D027]/[0.25] hover:outline hover:outline-offset-4 text-white transition-all ease-out duration-300">
                 <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-[#FCE894] opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
                 <span class="relative">Join the deck!</span>
             </a>
+        </div>
+
+        <div  v-else class="h-2/5 absolute right-[50px] flex text-white flex-row items-center gap-7 text-xl">
+            <a class="font-Poppins font-normal text-[30px] text-white hover:text-[#F8D027] transition-all ease-out duration-300"><fa icon="bars"/></a>
         </div>
 
 
     </div>
 
 </template>
+
+<script>
+
+export default ({
+    components: {
+    },
+    data() {
+      return {
+        screenW: 0
+      }
+    },
+    mounted() {
+        window.addEventListener("resize", this.myEventHandler);
+        this.screenW = window.innerWidth
+    },
+    unmounted() {
+        window.removeEventListener("resize", this.myEventHandler);
+    },
+    methods: {
+        myEventHandler(){
+            console.log("something happened")
+            this.screenW = window.innerWidth
+        }
+    }
+  });
+  </script>
--- src/components/HeroSection-comp.vue
+++ src/components/HeroSection-comp.vue
@@ -1,20 +1,20 @@
 <template>
 
-    <div class="h-max w-full bg-[#30342D] relative max-w-[1600px]">
+    <div class="h-max min-h-[600px] w-full bg-[#30342D] relative max-w-[1600px] overflow-hidden">
         
-        <video class="aboslute w-full opacity-80" autoplay="true" loop muted>
+        <video class="aboslute h-100 scale-y-[5] scale-x-[5] sm:scale-y-[4] sm:scale-x-[4] md:scale-y-[3] md:scale-x-[3] lg:scale-y-[2] lg:scale-x-[2] xl:scale-y-[1.5] xl:scale-x-[1.5] 2xl:scale-y-[1] 2xl:scale-x-[1] opacity-80" autoplay="true" loop muted>
             <source src="https://www.dropbox.com/s/ywdvvz00b631wa3/Video%20for%20CARD%20SITE.mp4?dl=1" type="video/mp4">
             Your browser does not support the video tag.
             </video>
 
-        <div class="absolute top-[40%] left-[200px]">
+        <div class="absolute top-[40%] left-[5rem]">
             <h1 class="text-white/[0.5] text-md font-Poppins font-medium">Est. 2022</h1>
-            <h1 class="text-white text-7xl font-Poppins font-semibold">C.A.R.D. Airsoft</h1>
-            <h2 class="text-[#F8D027] text-3xl font-Poppins font-medium">Combat and Reconnaissance Division</h2>
-            <h3 class="text-white/[0.7] text-xl pt-5 w-[600px] font-Poppins font-normal">En casual airsoft og hardcore milsim foreningen med med stort fokus på trivsel, og kammarat skab, hvor der er plads til alle... Kom frisk soldat!</h3>
+            <h1 class="text-white text-5xl lg:text-7xl font-Poppins font-semibold">C.A.R.D. Airsoft</h1>
+            <h2 class="text-[#F8D027] text-lg lg:text-3xl font-Poppins font-medium">Combat and Reconnaissance Division</h2>
+            <h3 class="text-white/[0.7] text-sm lg:text-xl pt-2 lg:pt-5 w-[370px] lg:w-[600px] font-Poppins font-normal">En casual airsoft og hardcore milsim foreningen med med stort fokus på trivsel, og kammarat skab, hvor der er plads til alle... Kom frisk soldat!</h3>
 
-            <div class="absolute bottom-[-60px] left-[100px]">
-                <img class="w-fit h-[400px] opacity-10" alt="CARDlogo-white" src="@/assets/cardlogo_whiteout.svg"/>
+            <div class="absolute bottom-[-60px] left-[50px] lg:bottom-[-60px] lg:left-[100px]">
+                <img class="w-fit h-[300px] lg:h-[400px] opacity-10" alt="CARDlogo-white" src="@/assets/cardlogo_whiteout.svg"/>
             </div>
             
         </div>
