866febea385b43726bb0a8bd014dea8ec509c92b
Christian Ørum <chri@bubbles.dk>
2023-03-02 17:20:35 +0100

    Experimental test comp;: Unlock slider

--- src/components/Footer-comp.vue
+++ src/components/Footer-comp.vue
@@ -1,9 +1,32 @@
 <template>
 
     <div class="h-auto w-full pt-10 pb-10 bg-[#202316] relative max-w-[1600px]">
-        <div class="h-[300px]">
+
             <div class="bg-[#F8D027] w-100 h-[4px]"></div>
-        </div>
+
+            <div class="w-100 h-auto flex flex-col justify-center items-center">
+
+                <div class="grid grid-cols-4 w-[90%] gap-4 h-[300px] pb-[10px]">
+
+                    <div class="w-100 h-100 bg-white col-start-1 col-span-1 opacity-20">
+
+                    </div>
+
+                    <div class="w-100 h-100 bg-white col-start-2 col-span-1 opacity-20">
+
+                    </div>
+
+                    <div class="w-100 h-100 bg-white col-start-3 col-span-1 opacity-20">
+
+                    </div>
+
+                    <div class="w-100 h-100 bg-white col-start-4 col-span-1 opacity-20">
+
+                    </div>
+
+                </div>
+
+            </div>
 
         <p class="text-white opacity-70 text-sm absolute left-[5%] bottom-[30px]">Copyright © 2023 C.A.R.D. Airsoft. Alle rettigheder forbeholdes.</p>
         <p class="text-white opacity-70 text-sm absolute right-[5%] bottom-[30px]">Webdev: Christian Ørum</p>
